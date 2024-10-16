8f7a986f15e49cc23d57a22ec6c32321b8a42973
jsacap <sanchoalegre@gmail.com>
2024-10-17 10:01:00 +1100

    removed old api routes

--- app/api/chat/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getLangchainResponse } from "@/utils/langchain";
-
-export async function POST(request: NextRequest) {
-  const { message } = await request.json();
-  const response = await getLangchainResponse(message);
-  return NextResponse.json({ reply: response });
-}
